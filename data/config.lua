3a2e27ab72541eefe8ecc5f0ed3ca4ffbb80deff
mohammadrassoul <rassoulmhs@gmail.com>
2017-07-29 19:44:18 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,47 @@
+do local _ = {
+  admins = {
+    {
+      252537266,
+      "@MR\\_Rseven"
+    },
+    {
+      242169529,
+      "@mamadrseven"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer_fa",
+    "banhammer",
+    "gpsettings_en",
+    "gpsettings_fa",
+    "helpenergy",
+    "expire",
+    "nerkh",
+    "azan",
+    "del",
+    "id",
+    "mute-time",
+    "cleanbot",
+    "CleanMember",
+    "lock_fosh",
+    "monshi",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "Invite",
+    "addkick",
+    "delall",
+    "fun",
+    "CleanDeletedAccount"
+  },
+  info_text = "》Beyond Reborn v6.0\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/telebombang2018/tele_bom_bang_new \n\n\n\n》Special thanks to :\n》beyond_permag_bombang\n\n》Our channel :\n》@RsEvEn%%@RsEvEn\n\n》Our website :\n》http://telebombang.blogfa.com\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    301412247
+  }
+}
+return _
+end
